f8efe8a3ab8fb19530d7fb82ba13c32bf0a73285
joyke <joyke@alumni.uv.es>
2026-02-08 02:50:36 +0100

    área del trapecio

--- Perime.R
+++ Perime.R
@@ -1,4 +1,10 @@
-Areas_cuadrado<-function(l){
-  return(4*l)
+Areas_cuadrado<-function(L){
+  return(4*L)
 }
+# Perímetro de un rectángulo de base b y altura h
+perimetro_rectangulo <- function(b, h) {
+  2 * (b + h)
+}
+
+pi<-3.14159
 
